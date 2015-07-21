44beb0c8cc1008d6af7dc69cdb24c5cd332d947f
Guilherme Dutra <guipereira07@gmail.com>
2015-07-21 14:38:57 -0300

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to crawler-contrib
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/crawler_contrib/core_test.clj
@@ -0,0 +1,7 @@
+(ns crawler-contrib.core-test
+  (:require [clojure.test :refer :all]
+            [crawler-contrib.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
