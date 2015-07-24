10ffe88b64df89ea97756e3a1fa4175978cdaa33
Guilherme Dutra <guipereira07@gmail.com>
2015-07-24 18:10:39 -0300

    Get list of repositories

--- src/crawler_contrib/core.clj
+++ src/crawler_contrib/core.clj
@@ -2,12 +2,7 @@
   (require [tentacles.repos :as repos]
            [tentacles.pulls :as pulls]))
 
-(defn group-users-by-total-contributions [contributions]
-  (cond
-    (nil? contributions) nil
-    (empty? contributions) nil
-    :else
-    (-> (map-users-to-total-commits contributions) sort-by-total-commits)))
+(def auth)
 
 (defn map-users-to-total-commits [contributions]
   (into {} (map (fn [contribution]
@@ -17,5 +12,15 @@
 (defn sort-by-total-commits [users-with-commits]
   (sort-by val > users-with-commits))
 
+(defn group-users-by-total-contributions [contributions]
+  (cond
+    (nil? contributions) nil
+    (empty? contributions) nil
+    :else
+    (-> (map-users-to-total-commits contributions) sort-by-total-commits)))
+
+(defn get-all-repositories-contributions []
+  (map (fn [repo] (repos/contributor-statistics (:login (:owner repo)) (:name repo) auth)) (take 3 (repos/all-repos auth))))
+
 (defn -main []
- (println (group-users-by-total-contributions (repos/contributor-statistics "rails" "rails" {:auth "guipdutra"}))))
+  (map (fn [contrib] (group-users-by-total-contributions contrib)) (get-all-repositories-contributions)))
