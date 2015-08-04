b65c67b227bb2daaf470afdeeac4a208189c12f1
Guilherme Dutra <guipereira07@gmail.com>
2015-08-04 19:04:14 -0300

    tmp

--- src/crawler_contrib/contributions.clj
+++ src/crawler_contrib/contributions.clj
@@ -2,10 +2,9 @@
   (:use crawler-contrib.github-api-wrapper))
 
 (defn map-users-to-total-commits [contributions]
-  (into {}
-        (apply merge-with + (map (fn [contribution]
-               (hash-map (:login (:author contribution)) (:total contribution)))
-             contributions))))
+  (apply merge-with + (map (fn [contribution]
+                             (hash-map (:login (:author contribution)) (:total contribution)))
+                           (remove nil? contributions))))
 
 (defn sort-by-total-commits [users-with-commits]
   (sort-by val > users-with-commits))
@@ -14,12 +13,12 @@
   (cond
     (nil? contributions) nil
     (empty? contributions) nil
-    :else (map-users-to-total-commits contributions)))
+    :else
+    (map-users-to-total-commits contributions)))
 
 (defn get-all-repositories-contributions [all-repositories]
   (map (fn [repo] (get-repository-statistics repo)) all-repositories))
 
 (defn group-all-contributions-by-user-for-all-repositories []
-  (sort-by-total-commits (into {}
-         (mapcat (fn [contrib] (group-users-by-total-contributions contrib))
-                                  (get-all-repositories-contributions (get-all-repositories))))))
+  (map (fn [contrib] (group-users-by-total-contributions contrib))
+       (get-all-repositories-contributions (get-all-repositories))))
