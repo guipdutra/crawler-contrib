8440b4118ba904104775960bf124956c5148cdef
Guilherme Dutra <guipereira07@gmail.com>
2015-08-10 17:56:18 -0300

    Wait until has request rate

--- src/crawler_contrib/github_api_wrapper.clj
+++ src/crawler_contrib/github_api_wrapper.clj
@@ -1,17 +1,23 @@
 (ns crawler-contrib.github-api-wrapper
   (require [tentacles.repos :as repos]
            [tentacles.users :as users]
+           [tentacles.core :as core]
            [tentacles.pulls :as pulls]))
 
 (def auth {:oauth-token (System/getenv "GITHUB_ACCESS_TOKEN")})
 
 (def options {:all-pages true})
 
+(defn make-request [method-to-call]
+  (if (> (:remaining (:core (:resources (core/rate-limit auth)))) 0)
+    (method-to-call)
+    (make-request method-to-call)))
+
 (defn get-all-repositories []
-  (take 300 (repos/all-repos (merge auth options))))
+  (take 2 (make-request (fn [] (repos/all-repos (merge auth options))))))
 
 (defn get-repository-statistics [repo]
-  (repos/contributor-statistics (:login (:owner repo)) (:name repo) auth))
+  (make-request (fn [] (repos/contributor-statistics (:login (:owner repo)) (:name repo) auth))))
 
 (defn get-user [user-name]
-  (users/user user-name auth))
+  (make-request (fn [] (users/user user-name auth))))
