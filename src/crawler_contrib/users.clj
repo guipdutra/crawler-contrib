23353498c1f149254de4a6e7678bc4ccdb0c88c8
Guilherme Dutra <guipereira07@gmail.com>
2015-08-10 13:27:10 -0300

    Now get user info first before filtering

--- src/crawler_contrib/users.clj
+++ src/crawler_contrib/users.clj
@@ -2,12 +2,28 @@
   (:require [crawler-contrib.github-api-wrapper
              :refer [get-user]]))
 
+(def defualt-locations
+  ["Belo Horizonte" "Brazil" "São Paulo"])
+
 (defn extract-location [user-info]
-  (let [location (:location (get-user user-info))]
+  (let [location (:location user-info)]
     (if (clojure.string/blank? location) "" location)))
 
+(def is-location-valid?
+  (fn [user location] (.contains (extract-location user) location)))
+
+(defn is-location-valid-for-user? [user]
+  (partial is-location-valid? user))
+
 (def is-brazilian?
-  (fn [user] (.contains (extract-location user) "Brazil")))
+  (fn [user]
+   (some (is-location-valid-for-user? user) defualt-locations)))
+
+(def extract-login
+  (fn [user-info] (:login user-info)))
+
+(def get-user-info
+ (fn [user-name] (get-user user-name)))
 
 (defn filter-by-brazilians [users]
-  (filter is-brazilian? users))
+  (map extract-login (filter is-brazilian? (pmap get-user-info users))))
--- test/crawler_contrib/users_test.clj
+++ test/crawler_contrib/users_test.clj
@@ -4,10 +4,18 @@
 
 (facts "about 'filter-by-brazilians'"
        (fact "returns only the brazilians one"
-             (filter-by-brazilians ["guipdutra" "rodrigomaia17" "wycats" "mviegas" "gregoriomelo"]) => ["guipdutra" "rodrigomaia17"]
+             (filter-by-brazilians ["guipdutra"
+                                    "rodrigomaia17"
+                                    "wycats"
+                                    "mviegas"
+                                    "gregoriomelo"
+                                    "ffrancieli"
+                                    "cv"]) => ["guipdutra" "rodrigomaia17" "ffrancieli" "cv"]
              (provided
-               (crawler-contrib.github-api-wrapper/get-user "guipdutra") => {:location "Porto Alegre, Brazil"}
-               (crawler-contrib.github-api-wrapper/get-user "rodrigomaia17") => {:location "Belo Horizonte, Brazil"}
-               (crawler-contrib.github-api-wrapper/get-user "mviegas") => {:location nil}
-               (crawler-contrib.github-api-wrapper/get-user "gregoriomelo") => {:location ""}
-               (crawler-contrib.github-api-wrapper/get-user "wycats") => {:location "Chicado, USA"})))
+               (crawler-contrib.github-api-wrapper/get-user "guipdutra") => {:location "Porto Alegre, Brazil" :login "guipdutra"}
+               (crawler-contrib.github-api-wrapper/get-user "rodrigomaia17") => {:location "Belo Horizonte, Brazil" :login "rodrigomaia17"}
+               (crawler-contrib.github-api-wrapper/get-user "mviegas") => {:location nil :login "mviegas"}
+               (crawler-contrib.github-api-wrapper/get-user "ffrancieli") => {:location "Belo Horizonte" :login "ffrancieli"}
+               (crawler-contrib.github-api-wrapper/get-user "gregoriomelo") => {:location "" :login "gregoriomelo"}
+               (crawler-contrib.github-api-wrapper/get-user "cv") => {:location "São Paulo" :login "cv"}
+               (crawler-contrib.github-api-wrapper/get-user "wycats") => {:location "Chicado, USA" :login "wycats"})))
