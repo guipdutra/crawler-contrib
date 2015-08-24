df2bb81c6295e171ae6f6f61a750d914e4739f0e
Guilherme Dutra <guipereira07@gmail.com>
2015-08-24 14:03:55 -0300

    Add dependencies to create server

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                    :plugins [[lein-midje "3.1.3"]]}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring/ring-core "1.4.0"]
+                 [clj-http "2.0.0"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [ring/ring-json "0.4.0"]
                  [tentacles "0.4.0"]]
